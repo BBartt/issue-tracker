1c529c2e60f0eafbb7290fc75f36a07f3e2c2ab5
Bartosz <bartek2@onet.eu>
2024-02-16 16:59:44 +0100

    Build an API for updating issues

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -16,22 +16,22 @@ const SimpleMDE = dynamic(async () => import("react-simplemde-editor"), {
 });
 import "easymde/dist/easymde.min.css";
 
-import { createIssueSchema } from "@/app/validationSchemas";
+import { issueSchema } from "@/app/validationSchemas";
 import { zodResolver } from "@hookform/resolvers/zod";
 import Spinner from "@/app/components/Spinner";
 import { ErrorMessage } from "@/app/components";
 import { Issue } from "@prisma/client";
 
-type IssueFormData = z.infer<typeof createIssueSchema>;
+type IssueFormData = z.infer<typeof issueSchema>;
 
-const IssueForm = ({ issue }: { issue: Issue }) => {
+const IssueForm = ({ issue }: { issue?: Issue }) => {
   const {
     register,
     control,
     handleSubmit,
     formState: { errors },
   } = useForm<IssueFormData>({
-    resolver: zodResolver(createIssueSchema),
+    resolver: zodResolver(issueSchema),
   });
   const router = useRouter();
   const [error, setError] = useState("");
